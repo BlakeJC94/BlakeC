c5fdfeebb9e25bf8b6706a0bb167ba0022f66339
BlakeJC94 <blakejamescook@gmail.com>
2016-01-19 11:54:45 +1100

    Finished T Cell spawner, configurable rate. Modified CellKiller. Remove ImmortalCellLabel and left immortal cell unlabelled instead, as it is uniquely identified by node_index = 0. Added TumorCell Label for better simulation component function.

---
+++ test/Week3Tasks/Archive/Stage2b/TCellSimulationTask2.hpp
@@ -0,0 +1,237 @@
+/*
+
+Copyright (c) 2005-2015, University of Oxford.
+All rights reserved.
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+
+This file is part of Chaste.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ * Neither the name of the University of Oxford nor the names of its
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+
+/* After Week 2, we have a basic, but working first iteration of the simulation we are
+ * working towards. This week, we wish to add more foundations for more adding more
+ * complexity in later revisions. This will require rewriting some components to be 
+ * more flexiblew with cell counts and a more sophisticted CellCycleModel for T Cells
+ * as well as a new label to protect the primary T Cell spawner. */
+
+/* Task 2. Write a new CellCycleModel to make the ImTCell the primary spawn for T Cells
+        a. Make new TCells and remove the ImTCell Property */ 
+
+
+#include <cxxtest/TestSuite.h>
+#include "CheckpointArchiveTypes.hpp"
+
+#include "AbstractCellBasedTestSuite.hpp"
+#include "PetscSetupAndFinalize.hpp"
+#include "CellsGenerator.hpp"
+#include "DifferentiatedCellProliferativeType.hpp"
+#include "OffLatticeSimulation.hpp"
+#include "SmartPointers.hpp"
+#include "NodesOnlyMesh.hpp"
+#include "NodeBasedCellPopulation.hpp"
+
+#include "TCellBoundaryCondition.hpp"
+#include "TCellDiffusionForce.hpp"
+#include "TCellTumorCellCycleModel.hpp"
+#include "TCellTumorCellKiller.hpp"
+#include "TCellTumorSpringForce.hpp"
+
+#include "TCellProperty.hpp"
+//#include "ImmortalCellProperty.hpp"
+#include "TumorCellProperty.hpp"
+
+
+class TCellSimulation : public AbstractCellBasedTestSuite
+{
+public:
+
+    void TestTCellSimulation() throw(Exception)
+    {
+    
+        // T Cell simulation intial state options
+        unsigned num_t_cells = 5;
+        unsigned num_tumor_cells = 4;
+        double initial_domain_radius = 5;
+        
+        std::vector<Node<2>*> nodes;
+        
+        
+        // Generate Immortal T Cell node (spawn controller for futre revs) 
+        //     node index = 0
+        nodes.push_back(new Node<2>(0, false, 6 , 6));
+        
+        // Generate T Cell nodes (random loactions in annular domain 2<r<5)
+        //     node index range = [1, num_t_cells]
+        RandomNumberGenerator* p_gen_r = RandomNumberGenerator::Instance();
+        RandomNumberGenerator* p_gen_theta = RandomNumberGenerator::Instance();
+        for (unsigned index = 0; index < num_t_cells; index++)
+        {            
+            double radial_coord = (initial_domain_radius - 2)*p_gen_r->ranf() + 2;
+            double angular_coord = p_gen_theta->ranf() * 6.283185307;
+            
+            nodes.push_back(new Node<2>(index, false, 
+                radial_coord * cos(angular_coord) ,  
+                radial_coord * sin(angular_coord)));
+        }
+        
+        // Generate Tumor Cell nodes (random loactions in circular domain r<1)
+        //     node index range = [num_t_cells + 1, num_t_cells + num_tumor_cells]
+        for (unsigned index = 0; index < num_tumor_cells; index++)
+        {
+            double radial_coord = p_gen_r->ranf();
+            double angular_coord = p_gen_theta->ranf() * 6.283185307;
+            
+            nodes.push_back(new Node<2>(index, false, 
+                radial_coord * cos(angular_coord) ,  
+                radial_coord * sin(angular_coord)));
+        }
+        
+        
+        // Generate mesh
+        NodesOnlyMesh<2> mesh;
+        mesh.ConstructNodesWithoutMesh(nodes, 1.5);
+        
+        
+        // Generate cells
+        std::vector<CellPtr> cells;
+        
+        MAKE_PTR(TransitCellProliferativeType, p_transit_type);
+        MAKE_PTR(WildTypeCellMutationState, p_state);
+        
+        MAKE_PTR_ARGS(CellLabel, p_t_cell_label, (3)); // Temporary measure to colour cells differently
+        MAKE_PTR(TCellProperty, p_t_cell_property); 
+        
+        /*
+        MAKE_PTR_ARGS(CellLabel, p_imm_cell_label, (5));
+        MAKE_PTR(ImmortalCellProperty, p_imm_cell_property); 
+        */
+        
+        MAKE_PTR_ARGS(CellLabel, p_tumor_cell_label, (5));
+        MAKE_PTR(TumorCellProperty, p_tumor_cell_property);
+        
+        for (unsigned i=0; i<mesh.GetNumNodes(); i++) 
+        {
+        
+            // Cell cycle model for all cells
+            //   Labelled (T Cells) do not divide
+            //   Unlabelled (Tumor Cells) divide according to a U[0,2] distribution
+            TCellTumorCellCycleModel* p_model = new TCellTumorCellCycleModel();
+            CellPropertyCollection collection;
+            
+            // Note, this labelling process is the ONLY place node index matters
+            /*
+            if (i <= num_t_cells)
+            {
+                if (i == 0)
+                {
+                    collection.AddProperty(p_imm_cell_label);
+                    collection.AddProperty(p_imm_cell_property);
+                }
+                else if (i <= num_t_cells)
+                {
+                    collection.AddProperty(p_t_cell_label);
+                    collection.AddProperty(p_t_cell_property);
+                }
+                //else
+                //{
+                //    collection.AddProperty(p_tumor_cell_property);
+                //}
+            }
+            */
+            
+            if (  (i <= num_t_cells) && (i != 0)  )
+            {
+               collection.AddProperty(p_t_cell_label);
+               collection.AddProperty(p_t_cell_property);
+            }
+            else if (i > num_t_cells)
+            {
+                collection.AddProperty(p_tumor_cell_label);
+                collection.AddProperty(p_tumor_cell_property);
+            }
+            
+            CellPtr p_cell(new Cell(p_state, p_model, NULL, false, collection));
+            p_cell->SetCellProliferativeType(p_transit_type);
+            
+            double birth_time = - RandomNumberGenerator::Instance()->ranf() *
+                (p_model->GetStemCellG1Duration()
+                    + p_model->GetSG2MDuration());
+            
+            p_cell->SetBirthTime(birth_time);
+            
+            cells.push_back(p_cell);
+            
+        }
+        
+        
+        // Generate Cell Population
+        NodeBasedCellPopulation<2> cell_population(mesh, cells);
+        
+        
+        // Begin OffLatticeSimulation
+        OffLatticeSimulation<2> simulator(cell_population);
+        simulator.SetOutputDirectory("TestTCellSimulation");
+        simulator.SetSamplingTimestepMultiple(6);
+        simulator.SetEndTime(45.0);
+
+
+        // Add generalised linear spring forces between cells
+        //   Tumor Cell & Tumor Cell --> Attraction and Repulsion
+        //   T Cell & T Cell --> Repulsion only
+        MAKE_PTR(TCellTumorSpringForce<2>, p_spring_force);
+        simulator.AddForce(p_spring_force);
+        
+        // Add diffusion force component and constant left-facing force componet to T Cells
+        MAKE_PTR(TCellDiffusionForce<2>, p_diffusion_force);
+        simulator.AddForce(p_diffusion_force);
+        
+        // Add "portal" boundary condition on edges of square region
+        MAKE_PTR_ARGS(TCellBoundaryCondition, p_bc, (&cell_population));
+        simulator.AddCellPopulationBoundaryCondition(p_bc);
+        
+        // Add cell killer (kills tumor cells instantly when a T Cell is nearby)
+        MAKE_PTR_ARGS(TCellTumorCellKiller, p_cell_killer, (&cell_population));
+        simulator.AddCellKiller(p_cell_killer);
+        
+
+        simulator.Solve();
+        
+    }
+};
+
+// Pass
+
+/* Issue to be addressed:
+ *
+ * Adding TCellProperty to daughter cells of the immortal cell at node 0 is 
+ * not enough to stop them dividing. This may call for a rework of the system
+ * which utilises the CellCycleModel proprties better (i.e. node 0 = stem cell
+ * --Divides--->Created new transit cell, no more division) */
+
