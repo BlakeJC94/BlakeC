c5499c9dad41b5871d7b004e04a0d1192014bf17
BlakeJC94 <blakejamescook@gmail.com>
2018-08-26 13:17:50 +1000

    Finished basic cap height typical sim plotting code
    TODO
    - histogram slope thingo

--- MATLAB/paper_typicalSim_plots.m
+++ MATLAB/paper_typicalSim_plots.m
@@ -3,11 +3,11 @@ function paper_typicalSim_plots
 close all;
 addpath(genpath('PlotData/functions'));
 
-testoutput_path = 'PlotData/data/typicalsims';
-data_path = [testoutput_path '/UtericBud_model_3_param_0.6_pa_0_pd_0_simtime_1000_'];
+testoutput_path = 'PlotData/data/paper/typicalsims3';
+data_path = [testoutput_path '/UtericBud_model_3_param_0.6_pa_0_pd_0_simtime_600_'];
 
 
-stop_time = 1000;
+stop_time = 600;
 
 stop_index = round(1666*stop_time/1000)+1;
 
@@ -96,6 +96,61 @@ xlabel('Simulation time'); ylabel('No. of cells');
 
 
 %% plot change in cap height
+sampleindexmax=0;
+prolif=0;
+height_data = cell(TotalJobs,1);
+for i = 1:TotalJobs
+    height_data{i} = zeros(20, 1);
+end
+
+
+plot_height_time = zeros(1,sampleindexmax);
+
+for k = 1:TotalJobs
+    disp(k)
+    loaddata = LoadNonConstantLengthData([data_path 'sim_' num2str(k-1) '/results_from_time_0/cellstate.dat']);
+    
+    if (k == 1) && (sampleindexmax == 0)
+        sampleindexmax = length(loaddata);        
+    end
+    
+    for i = 1:sampleindexmax %over each sample step
+        y = loaddata{i}(4:4:end-1);
+
+        if k == 1
+            plot_height_time(i) = loaddata{i}(1);
+        end
+
+        if prolif == 1
+            state = loaddata{i}(5:4:end);
+            tmp = (state ~= 0) .* y;
+            y = y(tmp>0);
+        end
+        tmp = sort(y,'descend');
+        if length(tmp) < 10
+            height_data{k}(i,1) = tmp(1);
+        else
+            height_data{k}(i,1) = tmp(10);
+        end
+    end
+    
+end
+
+
+
+plot_height = zeros(1,sampleindexmax);
+for i = 1:sampleindexmax
+    plot_height(i) = height_data{1}(i,1);
+    for k = 2:TotalJobs
+        plot_height(i) = (k-1)/k * (plot_height(i) + height_data{k}(i,1));
+    end
+end
+
+figure;
+plot(plot_height_time, plot_height)
+
+keyboard;
+
 
 
 
