1a2252b62664134f6351235ad0bae6fed281d83c
BlakeJC94 <blakejamescook@gmail.com>
2018-08-26 14:13:30 +1000

    Finished typicalsim plots

--- MATLAB/paper_typicalSim_plots.m
+++ MATLAB/paper_typicalSim_plots.m
@@ -6,6 +6,7 @@ addpath(genpath('PlotData/functions'));
 testoutput_path = 'PlotData/data/paper/typicalsims3';
 data_path = [testoutput_path '/UtericBud_model_3_param_0.6_pa_0_pd_0_simtime_600_'];
 
+figure_dir = 'Figures/Paper/typicalSim/';
 
 stop_time = 600;
 
@@ -51,16 +52,21 @@ for i = 1:TotalJobs
     
 end
 
-figure;
+fig = figure;
+fig.Units = 'centimeters';
+fig.Position = [10 10 20 15];
 
 plot(SimTime, TotalCells);
 
 title('Total cells across each simulation', 'Interpreter', 'latex');
-xlabel('Simulation time (h)'); ylabel('No. of cells');
+xlabel('Simulation time (h)', 'Interpreter', 'latex'); 
+ylabel('No. of cells', 'Interpreter', 'latex');
+
 
 axis([0 stop_time 0 350]);
 
-
+SaveAsPngEpsAndFig(-1, [figure_dir, 'typicalSim_pop'], ...
+    11, 7/5, 15);
 
 
 
@@ -78,7 +84,9 @@ TotalCells = MeanPopulationData(:,3) + MeanPopulationData(:,2);
 RVCells = MeanPopulationData(:,6);
 
 
-figure; 
+fig = figure;
+fig.Units = 'centimeters';
+fig.Position = [10 10 20 15];
 
 plot(SimTime, TotalCells, 'k',...
     SimTime, TransitCells, 'b',...
@@ -90,7 +98,11 @@ legend('Total', 'CM cells', 'RV Cells','Location', 'Best');
 
 % title(['Average number of cells over ', num2str(TotalJobs), ' simulations']);
 title('Average number of cells', 'Interpreter', 'latex');
-xlabel('Simulation time'); ylabel('No. of cells');
+xlabel('Simulation time (h)', 'Interpreter', 'latex'); 
+ylabel('No. of cells', 'Interpreter', 'latex');
+
+SaveAsPngEpsAndFig(-1, [figure_dir, 'typicalSim_avg_pop'], ...
+    11, 7/5, 15);
 
 
 
@@ -107,7 +119,7 @@ end
 plot_height_time = zeros(1,sampleindexmax);
 
 for k = 1:TotalJobs
-    disp(k)
+
     loaddata = LoadNonConstantLengthData([data_path 'sim_' num2str(k-1) '/results_from_time_0/cellstate.dat']);
     
     if (k == 1) && (sampleindexmax == 0)
@@ -146,10 +158,18 @@ for i = 1:sampleindexmax
     end
 end
 
-figure;
-plot(plot_height_time, plot_height)
+fig = figure;
+fig.Units = 'centimeters';
+fig.Position = [10 10 20 15];
 
-keyboard;
+plot(plot_height_time, plot_height, 'k');
+title('Average cap height', 'Interpreter', 'latex');
+xlabel('Simulation time (h)','Interpreter', 'latex');
+ylabel('Cap height (cd)','Interpreter', 'latex');
+
+SaveAsPngEpsAndFig(-1, [figure_dir, 'typicalSim_cap_height'], ...
+    11, 7/5, 15);
+
 
 
 
@@ -157,6 +177,89 @@ keyboard;
 
 
 %% plot change in histogram slopes (both total and proliferative on same plot?)
+histogram_data = cell(TotalJobs,1);
+histogram_data_prolif = cell(TotalJobs,1);
+for i = 1:TotalJobs
+    histogram_data{i} = zeros(21, 20);
+    histogram_data_prolif{i} = zeros(21, 20);
+end
+
+plot_slope_time = zeros(1,sampleindexmax);
+
+for k = 1:TotalJobs
+    
+    loaddata = LoadNonConstantLengthData([data_path 'sim_' num2str(k-1) '/results_from_time_0/cellstate.dat']);
+    
+    for i = 1:length(loaddata) % over each sample step
+        x = loaddata{i}(3:4:end-2);
+
+        if k == 1
+            plot_slope_time(i) = loaddata{i}(1);
+        end
+
+        histogram_data{k}(i,:) = histcounts(x,0:1:20, 'Normalization', 'probability');
+
+        state = loaddata{i}(5:4:end);
+        tmp = (state ~= 0) .* x;
+
+        histogram_data_prolif{k}(i,:) = histcounts(x(tmp>0),0:1:20, 'Normalization', 'probability');
+
+    end
+
+end
+
+
+plot_slope = zeros(1,sampleindexmax);
+plot_slope_prolif = zeros(1,sampleindexmax);
+m = 0.5:1:19.5;
+M = [ones(length(m),1), m'];
+for i = 1:sampleindexmax
+    n = histogram_data{1}(i,:);
+    b = M\n';
+    slope = b(2);
+
+    plot_slope(i) = slope;
+    for k = 2:TotalJobs
+        n = histogram_data{k}(i,:);
+        b = M\n';
+        slope = b(2);
+
+        plot_slope(i) = (k-1)/k * (plot_slope(i) + slope);
+    end
+
+
+    n = histogram_data_prolif{1}(i,:);
+    b = M\n';
+    slope = b(2);
+
+    plot_slope_prolif(i) = slope;
+    for k = 2:TotalJobs
+        n = histogram_data_prolif{k}(i,:);
+        b = M\n';
+        slope = b(2);
+
+        plot_slope_prolif(i) = (k-1)/k * (plot_slope(i) + slope);
+    end
+end
+
+
+fig = figure;
+fig.Units = 'centimeters';
+fig.Position = [10 10 20 15];
+
+
+plot(...
+    plot_slope_time, plot_slope, 'k', ...
+    plot_slope_time, plot_slope_prolif, 'b' ...
+    );
+title('Average histogram slope', 'Interpreter', 'latex');
+xlabel('Simulation time (h)' ,'Interpreter', 'latex');
+ylabel('Histogram slope','Interpreter', 'latex');
+legend('Total', 'CM cells', 'Location', 'Best');
+
+SaveAsPngEpsAndFig(-1, [figure_dir, 'typicalSim_hist_slope'], ...
+    11, 7/5, 15);
+
 
 
 
